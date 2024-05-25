2e99b303d6fca990f2d7e838e6d6232f76f6a7b8
jelly3105 <anjalisherikar3105@gmail.com>
2024-05-25 10:37:06 +0530

    access req body and header

--- 01-Server/index.ts
+++ 01-Server/index.ts
@@ -1,8 +1,16 @@
 import express from "express";
 const app = express();
 
+app.use(express.json()); // To access request body
+
 app.get("/", (req,res) => {
     res.send("Hello World!")
+})
+
+app.post("/conversation", (req,res) => {
+    console.log(req.headers)
+    console.log(req.body)
+    res.send("Request received!")
 })
 
 app.listen(3000, () => {
